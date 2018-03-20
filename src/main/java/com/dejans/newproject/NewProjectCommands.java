8fa2742d16b2c1efcfa6d7f67603b8881a264a30
dejansmi <smiljanic_dejan@hotmail.com>
2018-03-20 10:43:59 +0100

    8

--- src/main/java/com/dejans/newproject/NewProjectCommands.java
+++ src/main/java/com/dejans/newproject/NewProjectCommands.java
@@ -2,9 +2,11 @@ package com.dejans.newproject;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import freemarker.template.Configuration;
@@ -12,16 +14,24 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 
-
 public class NewProjectCommands {
     private String[] args;
-    String baseDir;
+    private String baseDir;
     String baseDirProject;
     String nameProject;
+    // Short name of project witout special character (-,#,$). It use as a java variable
+    String name;
+    boolean zipSet;
 
     public NewProjectCommands(String[] args) {
         this.args = args;
         this.baseDir = System.getenv("JAVA_PROJECTS_BASE");
+        zipSet = false;
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("--zip")) {
+                zipSet = true;
+            }
+        }
     }
 
     public void execute() {
@@ -31,6 +41,17 @@ public class NewProjectCommands {
         }
         nameProject = args[0];
         baseDirProject = baseDir + "/source/" + nameProject;
+        name = nameProject;
+        while (name.contains("-") || name.contains(" ") || name.contains("#")) {
+            int ind = name.indexOf("-");
+            if (ind > -1) {
+                name = name.substring(0, 1).toUpperCase()
+                        + (name.substring(0, ind).length() > 1 ? name.substring(1, ind) : "")
+                        + (name.substring(ind + 1).length() > 0 ? name.substring(ind + 1, ind + 2).toUpperCase()
+                                + (name.substring(ind + 1).length() > 1 ? name.substring(ind + 2) : "") : "");
+            }
+        }
+
         boolean de = new File(baseDirProject).exists();
         if (de) {
             System.out.println("Project (directory or file) " + nameProject + " exists.");
@@ -41,12 +62,16 @@ public class NewProjectCommands {
                 //TODO: SR: napraviti gresku 
             }
             try {
-                Zip zip = new Zip();
-                zip.zipDir(baseDir + "/new-project-template", baseDirProject + "/temp.zip");
-                zip.unzip(baseDirProject, "/temp.zip", baseDirProject);
-                File zipDel = new File(baseDirProject + "/temp.zip");
-                zipDel.delete();
-                File subFile = new File(baseDirProject);
+                File destFile = new File(baseDirProject);
+                if (zipSet) {
+                    Zip zip = new Zip();
+                    zip.zipDir(baseDir + "/new-project-template", baseDirProject + "/temp.zip");
+                    zip.unzip(baseDirProject, "/temp.zip", baseDirProject);
+                    File zipDel = new File(baseDirProject + "/temp.zip");
+                    zipDel.delete();
+                } else {
+                    FileUtils.touch(destFile);
+                }
                 /* Create and adjust the configuration singleton */
                 Configuration cfg = new Configuration(Configuration.VERSION_2_3_27);
                 //cfg.setDirectoryForTemplateLoading(new File("Files"));
@@ -55,7 +80,14 @@ public class NewProjectCommands {
                 cfg.setLogTemplateExceptions(false);
                 cfg.setWrapUncheckedExceptions(true);
 
-                subdirecotories(subFile, cfg);
+                Map root = new HashMap();
+                root.put("basedir", "${basedir}");
+                root.put("name", name);
+                root.put("nameProject", nameProject);
+        
+
+                File sourceFile = new File(baseDir + "/new-project-template");
+                subdirecotories(sourceFile, destFile, baseDir + "/new-project-template", baseDirProject, cfg, root);
             } catch (IOException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
@@ -63,45 +95,37 @@ public class NewProjectCommands {
         }
     }
 
-    private void subdirecotories(File fileCurrent, Configuration cfg) throws IOException {
+    private void subdirecotories(File fileCurrent, File destFile, String sourceBase, String destinationBase, Configuration cfg, Map root)
+            throws IOException {
         if (fileCurrent.isDirectory()) {
             File[] children = fileCurrent.listFiles();
             for (File childFile : children) {
-                subdirecotories(childFile, cfg);
+                // First change name of files (if there are have $name$)
+                File dstFile = new File(childFile.getPath().replace(sourceBase, destinationBase));
+                if (dstFile.getName() != null && dstFile.getName().contains("$name")) {
+                    String newName = dstFile.getPath().replace("$name$", name);
+                    dstFile = new File(newName);
+                }
+                if (childFile.isDirectory()) {
+                    dstFile.mkdir();
+                } else {
+                    FileUtils.touch(dstFile);
+                }
+                subdirecotories(childFile, dstFile, sourceBase, destinationBase, cfg, root);
             }
             return;
-        }
-        // First change name of files (if there are have $name$)
-        if (fileCurrent.getName()!= null && fileCurrent.getName().contains("$name")) {
-            String newName = nameProject;
-            while (newName.contains("-") || newName.contains(" ") || newName.contains("#")) {
-                int ind = newName.indexOf("-");
-                if (ind > -1) {
-                    newName = newName.substring(0,1).toUpperCase() 
-                            + (newName.substring(0,ind).length()>1 ?newName.substring(1,ind):"") 
-                            + (newName.substring(ind+1).length()>0?newName.substring(ind+1,ind+2).toUpperCase()
-                            + (newName.substring(ind+1).length()>1?newName.substring(ind+2):""):"");
-                }
-            }
-            newName = fileCurrent.getName().replace("$name$", newName);
-            File newFile = new File(fileCurrent.getParentFile(),newName);
-            fileCurrent.renameTo(newFile);
-            fileCurrent = new File(fileCurrent.getParentFile(), newName);
         }
         /* Get the template (uses cache internally) */
         cfg.setDirectoryForTemplateLoading(fileCurrent.getParentFile());
         Template temp = cfg.getTemplate(fileCurrent.getName());
 
-        File dir = new File(fileCurrent.getPath()+"XXX");
-        BufferedWriter out = new BufferedWriter(new FileWriter(dir, false));
-        Map root = new HashMap();
-        root.put ("basedir","${basedir}");
+        BufferedWriter out = new BufferedWriter(new FileWriter(destFile, false));
         try {
-			temp.process(root, out);
-		} catch (TemplateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            temp.process(root, out);
+        } catch (TemplateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
         String st1 = fileCurrent.getPath();
         String st2 = fileCurrent.getParent();
